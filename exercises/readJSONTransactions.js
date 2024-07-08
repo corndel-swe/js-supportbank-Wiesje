c1190b94281ba04eab444f81d59de7ef8ba2ca48
wiesje <wiesje.heerik@corndel.com>
2024-07-08 10:59:08 +0100

    Solves d3e2

--- exercises/readJSONTransactions.js
+++ exercises/readJSONTransactions.js
@@ -2,6 +2,12 @@ import fs from 'fs/promises'
 
 export async function readJSONTransactions() {
   // Read the Transactions2013.json file
+  const path = new URL('../data/Transactions2013.json', import.meta.url)
+  const data = await fs.readFile(path)
+
   // Use JSON.parse to convert the string into an array of objects
+  const obj = JSON.parse(data)
+
   // and return it
+  return obj
 }
