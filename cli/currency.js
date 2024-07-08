b5acfabcf10094cff1a61dd0904a37c327c3a17b
wiesje <wiesje.heerik@corndel.com>
2024-07-08 17:43:41 +0100

    Workshop d3 replace hardcoded with API exchange rate

--- cli/currency.js
+++ cli/currency.js
@@ -1,15 +1,25 @@
 import { Command } from "commander";
+import 'dotenv/config'
+
 
 const currencyController = new Command('currency')
 
 currencyController
     .command('convert <amount> <from> <to>')
     .description('return equivalent amount in different currency')
-    .action((amount, from, to) => {
+    .action(async (amount, from, to) => {
         let conversionRate = 0
 
         if (from === 'usd' && to === 'gbp') {
-            conversionRate = 0.78
+            const domain = 'https://openexchangerates.org'
+            const endpoint = '/api/latest.json'
+            const url = new URL(endpoint, domain)
+            url.searchParams.append('app_id', process.env.OPENEXCHANGERATES_API_KEY)
+
+            const response = await fetch(url)
+            const data = await response.json()
+
+            conversionRate = data.rates.GBP
         }
 
         console.log(amount * conversionRate)
