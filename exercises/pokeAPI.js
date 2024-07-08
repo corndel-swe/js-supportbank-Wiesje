de9830af579e02810d66aa332e65e956cb428508
wiesje <wiesje.heerik@corndel.com>
2024-07-08 17:07:01 +0100

    Solves d3e3

--- exercises/pokeAPI.js
+++ exercises/pokeAPI.js
@@ -1,3 +1,9 @@
 export async function getPokemonList() {
-  // TODO
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+  const data = await response.json()
+  console.log(data)
+
+  const names = data.results.map(pokemon => pokemon.name)
+
+  return names
 }
